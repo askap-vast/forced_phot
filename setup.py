1b5e6cccf5e2f3dcb20d0cda8b9f7eb61d3e891d
Andrew O'Brien <obrienan@uwm.edu>
2020-11-02 16:40:48 -0600

    added requirements to setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,12 @@ setup(
     description="Simple forced photometry on FITS images with optional source clustering.",
     author="David Kaplan",
     author_email="kaplan@uwm.edu",
-    url="",
+    url="https://github.com/askap-vast/forced_phot",
     packages=find_packages(exclude=("tests")),
+    install_requires=[
+        "numpy",
+        "scipy",
+        "astropy",
+        "pandas",
+    ],
 )
