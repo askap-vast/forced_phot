74cef3a10830e8946aae591879df2c47274be5b6
Dougal Dobie <ddobie@swin.edu.au>
2024-11-20 13:44:15 +1100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="forced_phot",
-    version="0.1.0",
+    version="0.2.0",
     description="Simple forced photometry on FITS images with optional source clustering.",
     author="David Kaplan",
     author_email="kaplan@uwm.edu",
